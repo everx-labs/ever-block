787f90330dd1b0700f9bb78b92715bd786d1a46a
Tonjen <jenkins@tonlabs.io>
2020-05-13 17:53:05 +0000

    automatic merge RC

--- src/bintree.rs
+++ src/bintree.rs
@@ -3,7 +3,6 @@
 *
 * Licensed under the SOFTWARE EVALUATION License (the "License"); you may not use
 * this file except in compliance with the License.
-*
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
