79c034152ffa371c468ec101b6e78fc9e707d2da
ViacheslavB <bvscd@mail.ru>
2024-01-15 19:23:50 +0300

    Merge pull request #551 from tonlabs/new-cap-nosplit
    Add new capability for disabling split of out queues during shard split #none

--- src/config_params.rs
+++ src/config_params.rs
@@ -390,6 +390,7 @@ pub enum GlobalCapabilities {
     CapFastFinality           = 0x0001_0000_0000,
     CapTvmV19                 = 0x0002_0000_0000, // TVM v1.9.x improvemements
     CapSmft                   = 0x0004_0000_0000,
+    CapNoSplitOutQueue        = 0x0008_0000_0000, // Don't split out queue on shard splitting
 }
 
 impl ConfigParams {
