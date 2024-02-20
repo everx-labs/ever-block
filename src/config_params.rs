e7261806916d68b7afcf76130d5a0d5aea791fbb
Sumrachek <zavarovskii@ya.ru>
2024-02-20 13:03:24 +0400

    Fixed CapCommonMessage and CapNoSplitOutQueue mismatch

--- src/config_params.rs
+++ src/config_params.rs
@@ -404,8 +404,8 @@ pub enum GlobalCapabilities {
     CapFastFinality           = 0x0001_0000_0000,
     CapTvmV19                 = 0x0002_0000_0000, // TVM v1.9.x improvemements
     CapSmft                   = 0x0004_0000_0000,
-    CapCommonMessage          = 0x0008_0000_0000,
-    CapNoSplitOutQueue        = 0x0010_0000_0000, // Don't split out queue on shard splitting
+    CapNoSplitOutQueue        = 0x0008_0000_0000, // Don't split out queue on shard splitting
+    CapCommonMessage          = 0x0010_0000_0000,
 }
 
 impl ConfigParams {
